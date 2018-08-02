acd1b2fba0cce03d9de4118176c26db6efc0676c
LAPTOP-VF5PAEIF\Aschw <aschwartz07901@gmail.com>
2018-08-02 06:39:49 -0600

    JS Excercise 3 & 4

--- JS_L_1.js
+++ JS_L_1.js
@@ -5,3 +5,10 @@ console.log('JavaScript');
 console.log(33.7);
 console.log(true);
 console.log(null);
+
+console.log(23+3.5);
+console.log(2018-1969);
+console.log(65/240);
+console.log(0.2708*100);
+
+console.log('Teaching the world how to code'.length);
