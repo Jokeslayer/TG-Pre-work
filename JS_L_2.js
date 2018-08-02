2313f766f82e967f25c894d41ac7e918728f8361
LAPTOP-VF5PAEIF\Aschw <aschwartz07901@gmail.com>
2018-08-02 07:02:19 -0600

    JS L1 Excercises 1-4

--- JS_L_2.js
+++ JS_L_2.js
@@ -10,3 +10,24 @@ let inNorthernHemisphere = true;
 console.log(location);
 console.log(latitude);
 console.log(inNorthernHemisphere);
+
+const entree = 'Enchiladas';
+
+const price = 12;
+
+console.log(entree);
+console.log(price);
+
+entree = 'Tacos';
+
+let changeMe = true;
+
+changeMe = false;
+
+let notDefined;
+
+console.log(notDefined);
+
+let valueless;
+
+console.log(valueless);
