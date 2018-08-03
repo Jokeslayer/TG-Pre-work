f1e1fb96b8f49e0bbefe35de0b1740dca4ff3780
LAPTOP-VF5PAEIF\Aschw <aschwartz07901@gmail.com>
2018-08-03 01:42:07 -0600

    JS L9 Excercise 11

--- JS_L_9.js
+++ JS_L_9.js
@@ -36,3 +36,18 @@ console.log(person[alarm]);
 console.log(person['hobbies']);
 console.log(person.sayHello());
 console.log(friend.sayHello());
+
+let person = {
+  _name: 'Lu Xun',
+  _age: 137,
+
+  set age(ageIn) {
+    if (typeof ageIn === 'number') {
+      this._age = ageIn;
+    }
+    else {
+      console.log('Invalid input');
+      return 'Invalid input';
+    }
+  }
+};
