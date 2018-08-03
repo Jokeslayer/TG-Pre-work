c4b84681ccfb4cd1bd9cad663d2eb4e4f5e013bd
LAPTOP-VF5PAEIF\Aschw <aschwartz07901@gmail.com>
2018-08-03 02:09:25 -0600

    JS L10 Excercise 4

--- JS_L_10.js
+++ JS_L_10.js
@@ -23,12 +23,29 @@ halley.incrementBehavior(); // Add one to behavior
 console.log(halley.name); // Print name value to console
 console.log(halley.behavior); // Print behavior value to console
 
-class Surgeon{
-  constructor(name, department){
+class Surgeon {
+  constructor(name, department) {
     this._name = name;
     this._department = department;
+    this._remainingVacationDays = 20;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get department() {
+    return this._department;
+  }
+
+  get remainingVacationDays() {
+    return this._remainingVacationDays;
+  }
+
+  takeVacationDays(daysOff) {
+    this._remainingVacationDays -= daysOff;
   }
 }
 
-const surgeonCurry = new Surgeon('Curry','Cardiovascular');
-const surgeonDurant = new Surgeon('Durant','Orthopedics');
+const surgeonCurry = new Surgeon('Curry', 'Cardiovascular');
+const surgeonDurant = new Surgeon('Durant', 'Orthopedics');
