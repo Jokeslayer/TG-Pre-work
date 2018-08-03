d998c1b0bd765b9f1fec62ed23dd7e49c6e9d1fa
LAPTOP-VF5PAEIF\Aschw <aschwartz07901@gmail.com>
2018-08-03 01:00:33 -0600

    JS L8 Excercise 5

--- JS_L_8.js
+++ JS_L_8.js
@@ -32,3 +32,19 @@ console.log(words.some(function(word) {
 let interestingWords = words.filter(word => word.length > 5);
 
 console.log(interestingWords.every(word =>  word.length > 5));
+
+let cities = ['Nashville', 'Charlotte', 'Asheville', 'Austin', 'Boulder'];
+
+let nums = [1, 50, 75, 200, 350, 525, 1000];
+
+//  Choose a method that will return undefined
+cities.forEach(city => console.log('Have you visited ' + city + '?'));
+
+// Choose a method that will return a new array
+let longCities = cities.filter(city => city.length > 7);
+
+// Choose a method that will return a new array
+let smallerNums = nums.map(num => num - 5);
+
+// Choose a method that will return a boolean value
+nums.every(num => num < 0);
