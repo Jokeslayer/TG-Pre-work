44e225fd537f60a3e7f217e12f3390952ec4bdad
LAPTOP-VF5PAEIF\Aschw <aschwartz07901@gmail.com>
2018-08-03 00:27:08 -0600

    JS L7 Excercise 5

--- JS_L_7.js
+++ JS_L_7.js
@@ -15,3 +15,15 @@ let vacationSpots = ['Venice', 'Athens', 'Florence']
 for(let vacationSpotIndex = vacationSpots.length-1;vacationSpotIndex >= 0 ;vacationSpotIndex--){
   console.log('I would love to visit '+vacationSpots[vacationSpotIndex]);
 }
+
+let myPlaces = ['San Francisco','Silicon Valley','Denver'];
+
+let friendPlaces = ['New York','Denver','Hawaii'];
+
+for (let myPlacesIndex = 0; myPlacesIndex < myPlaces.length; myPlacesIndex++){
+  	for (let friendPlacesIndex=0;friendPlacesIndex < friendPlaces.length;friendPlacesIndex++){
+      if (myPlaces[myPlacesIndex] === friendPlaces[friendPlacesIndex]){
+        console.log('Match: '+ friendPlaces[friendPlacesIndex]);
+      }
+    }
+}
