0b04d4984c0aa527425b5c709f626d9f8bfcce6e
LAPTOP-VF5PAEIF\Aschw <aschwartz07901@gmail.com>
2018-08-02 23:51:50 -0600

    JS L6 Excercise 3

--- JS_L_6.js
+++ JS_L_6.js
@@ -5,3 +5,9 @@ console.log(bucketList);
 let newYearsResolutions = ['make friends', 'learn more coding languages', 'get a job'];
 
 console.log(newYearsResolutions);
+
+let listItem = newYearsResolutions[0];
+
+console.log(newYearsResolutions[2]);
+
+console.log(newYearsResolutions[3]);
