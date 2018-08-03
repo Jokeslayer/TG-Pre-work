970bd125ca1fe2cea790a2d6130099d66d08c5ca
LAPTOP-VF5PAEIF\Aschw <aschwartz07901@gmail.com>
2018-08-02 23:40:24 -0600

    JS L5 excercise 3

--- JS_L_5.js
+++ JS_L_5.js
@@ -4,7 +4,24 @@ const galaxy = 'The Milky Way';
 let stars = 'North Star';
 
 const myNightSky = () => {
+  stars = 'Sirius';
   return 'Night Sky: ' + satellite + ', ' + stars + ', ' + galaxy;
 };
 
 console.log(myNightSky());
+
+console.log(stars);
+
+const visibleLightWaves = () => {
+  let lightWaves = 'Moonlight';
+  let region = 'The Arctic';
+    if (region === 'The Arctic') {
+      let lightWaves = 'Northern Lights';
+      console.log(lightWaves);
+    }
+  console.log(lightWaves);
+};
+
+visibleLightWaves();
+
+console.log(lightWaves);
