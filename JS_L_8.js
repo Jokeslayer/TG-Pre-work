9bfbd6633e620f4270db559381a741d4ea3a3ded
LAPTOP-VF5PAEIF\Aschw <aschwartz07901@gmail.com>
2018-08-03 00:45:00 -0600

    JS L8 Excercise 2

--- JS_L_8.js
+++ JS_L_8.js
@@ -1,3 +1,17 @@
 let fruits = ['mango', 'papaya', 'pineapple', 'apple'];
 
 fruits.forEach(fruit => console.log('I want to eat a '+fruit));
+
+let animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
+
+// Create the secretMessage array below
+let secretMessage = animals.map(animals => animals[0]);
+
+console.log(secretMessage.join(''));
+
+let bigNumbers = [100, 200, 300, 400, 500];
+
+// Create the smallNumbers array below
+let smallNumbers = bigNumbers.map(function(number){
+  return bigNumbers[number]/100;
+});
