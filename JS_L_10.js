5599f4a5a57621de6d4f11e5da3fb8ac70a35e1d
LAPTOP-VF5PAEIF\Aschw <aschwartz07901@gmail.com>
2018-08-03 02:03:14 -0600

    JS L10 Excercise 3

--- JS_L_10.js
+++ JS_L_10.js
@@ -29,3 +29,6 @@ class Surgeon{
     this._department = department;
   }
 }
+
+const surgeonCurry = new Surgeon('Curry','Cardiovascular');
+const surgeonDurant = new Surgeon('Durant','Orthopedics');
