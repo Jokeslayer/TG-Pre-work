c0c8e85582560220ae40920b1aca654f527ba19b
LAPTOP-VF5PAEIF\Aschw <aschwartz07901@gmail.com>
2018-08-02 23:49:02 -0600

    JS L6 Excercise 2

--- JS_L_6.js
+++ JS_L_6.js
@@ -1,3 +1,7 @@
 let bucketList = ['Rappel into a cave', 'Take a falconry class', 'Learn to juggle'];
 
 console.log(bucketList);
+
+let newYearsResolutions = ['make friends', 'learn more coding languages', 'get a job'];
+
+console.log(newYearsResolutions);
