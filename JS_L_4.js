f82937dc0a437fc2ecb381a7fbc306ddc60ca6c8
LAPTOP-VF5PAEIF\Aschw <aschwartz07901@gmail.com>
2018-08-02 23:29:02 -0600

    JS L4 Exercise 9

--- JS_L_4.js
+++ JS_L_4.js
@@ -64,3 +64,7 @@ const isGreaterThan = (numberOne, numberTwo) =>{
     return false;
   }
 };
+
+const volumeOfSphere = diameter => (1/6) * Math.PI * diameter * diameter * diameter;
+
+console.log('The volume of a sphere is ' + volumeOfSphere(10) + ' cubic centimeters');
