44b26cea1efdf2ced35e84971101e11d419126e5
LAPTOP-VF5PAEIF\Aschw <aschwartz07901@gmail.com>
2018-08-08 04:20:34 -0600

    JS L13 Excercise 6

---
+++ JS_L_13_ex_6_main.js
@@ -0,0 +1,39 @@
+// Information to reach API
+const apiKey = 'cdabf41c2997491c883c4038dcff346e';
+const url = 'https://api.rebrandly.com/v1/links';
+
+// Some page elements
+const inputField = document.querySelector('#input');
+const shortenButton = document.querySelector('#shorten');
+const responseField = document.querySelector('#responseField');
+
+// AJAX functions
+const shortenUrl = () => {
+  const urlToShorten = inputField.value;
+  const data = JSON.stringify({destination: urlToShorten});
+  const xhr = new XMLHttpRequest();
+  xhr.responseType = 'json';
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      renderResponse(xhr.response);
+    }
+	}
+  xhr.open('POST',url);
+  xhr.setRequestHeader('Content-type', 'application/json');
+	xhr.setRequestHeader('apikey', apiKey);
+  xhr.send(data);
+}
+
+const xhr = new XMLHttpRequest();
+
+
+// Clear page and call AJAX functions
+const displayShortUrl = (event) => {
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild);
+  }
+  shortenUrl();
+}
+
+shortenButton.addEventListener('click', displayShortUrl);
