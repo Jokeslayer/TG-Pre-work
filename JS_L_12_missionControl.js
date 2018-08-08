31b96559f9eee1f47e638207bb16b8521a52a93e
LAPTOP-VF5PAEIF\Aschw <aschwartz07901@gmail.com>
2018-08-08 03:13:48 -0600

    JS L12 Excercise 7

--- JS_L_12_missionControl.js
+++ JS_L_12_missionControl.js
@@ -1,9 +1,17 @@
-import Airplane from './JS_L_12_airplane.js';
+import {availableAirplanes, flightRequirements, meetsStaffRequirements} from './JS_L_12_airplane.js';
 
 function displayFuelCapacity() {
-  Airplane.availableAirplanes.forEach(function(element){
+  availableAirplanes.forEach(function(element){
   console.log('Fuel Capacity of ' + element.name + ': ' + element.fuelCapacity);
   });
 }
 
 displayFuelCapacity();
+
+function displayStaffStatus(){
+  availableAirplanes.forEach(function(element){
+    console.log(element.name + ' meets staff requirements: ' + 		meetsStaffRequirements(element.availableStaff, flightRequirements.requiredStaff) );
+  });
+};
+
+displayStaffStatus();
