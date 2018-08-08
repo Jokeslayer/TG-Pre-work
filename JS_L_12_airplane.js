f7d27eddc15a3a25ed3a92b48689cdd3fbed72fb
LAPTOP-VF5PAEIF\Aschw <aschwartz07901@gmail.com>
2018-08-08 03:20:01 -0600

    JS L12 Excercise 8

--- JS_L_12_airplane.js
+++ JS_L_12_airplane.js
@@ -1,24 +1,37 @@
-let availableAirplanes = [{
- name: 'AeroJet',
- fuelCapacity: 800,
- availableStaff: ['pilots', 'flightAttendants', 'engineers', 'medicalAssistance', 'sensorOperators'],
+export let availableAirplanes = [{
+name: 'AeroJet',
+fuelCapacity: 800,
+availableStaff: ['pilots', 'flightAttendants', 'engineers', 'medicalAssistance', 'sensorOperators'],
+maxSpeed: 1200,
+minSpeed: 300,
 },
 {name: 'SkyJet',
- fuelCapacity: 500,
- availableStaff: ['pilots', 'flightAttendants']
+fuelCapacity: 500,
+availableStaff: ['pilots', 'flightAttendants'],
+maxSpeed: 800,
+minSpeed: 200,
 }];
 
-let flightRequirements = {
-  requiredStaff: 4,
+export let flightRequirements = {
+ requiredStaff: 4,
+ requiredSpeedRange: 700,
 };
 
-function meetsStaffRequirements(availableStaff, requiredStaff) {
-  if(availableStaff.length >= requiredStaff){
-    return true;
-  }
-  else{
-    return false;
-  }
+export function meetsStaffRequirements(availableStaff, requiredStaff) {
+ if(availableStaff.length >= requiredStaff){
+   return true;
+ }
+ else{
+   return false;
+ }
 };
 
-export{availableAirplanes,flightRequirements, meetsStaffRequirements};
+export function meetsSpeedRangeRequirements(maxSpeed,minSpeed,requiredSpeedRange){
+ let range = maxSpeed - minSpeed;
+ if(range > requiredSpeedRange){
+   return true;
+ }
+ else{
+   return false;
+ }
+};
